87f13753e2575376f278cb5a0125006c3816a7e1
Lorenzo <lorenzo.bert99@gmail.com>
2019-10-15 15:53:08 +0200

    Creato progetto

---
+++ Progetto Cloud/Progetto Cloud/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Progetto_Cloud
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Progetto Cloud/Progetto Cloud/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Progetto_Cloud.MvcApplication" Language="C#" %>
